dd468e6c79e62fa7cf5499d4d27edffe70bb18e7
wojwoz <61656116+wojwoz@users.noreply.github.com>
2020-07-21 01:50:55 +0200

    todo2

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,16 @@
-function addNewItem()
-{
-    var listItem - document.captureEvents("")
-}
+function addNewItem(list, intemText) {
+    var listItem = document.createElement("li");
+    listItem.innerText = intemText;
+    list.appendChild(listItem);
+};
+
 var btnNew = document.getElementById("btnAdd");
-btnNew.onclick = function()
+btnNew.onclick = function() {
+    var itemText = prompt("new item?");
+    if(!intemText || intemText == "")
+    {
+        return false;
+    }
+
+    addNewItem(document.getElementById("todolist"));
+}; 
