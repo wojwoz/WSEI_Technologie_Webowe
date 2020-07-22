df19f38f3a4cfdf771b0cdc90398bb1076590457
wojwoz <61656116+wojwoz@users.noreply.github.com>
2020-07-22 08:46:57 +0200

    final

--- js/todo.js
+++ js/todo.js
@@ -1,53 +1,20 @@
-function updateItemStatus(){
-    var cbID = this.id.replace(cb_, "");
-    var itemText = document.getElementById("item_" + cbID);
-    
-    
+let dodaj = document.getElementById("dodaj");
+let lista = document.getElementById("todoContainter");
+let wpisz = document.getElementById("inputField");
 
-    if(this.checked) {
-        itemText.style.textDecoration ="line-trought";
-
-    }else {
-        itemText.style.textDecoration ="none    ";
-
-    }
-}
-function addNewItem(list, itemText) {
-    totalItems++;
-    var listItem = document.createElement("li");
-    var checkBox = document.createElement("input"); 
-    checkBox.type = "checkbox";
-    checkBox.id = "cb_" + totalItems;
-    checkBox.onclick = updateItemStatus;
+dodaj.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add("paragraph-styling");
+    paragraph.innerText = wpisz.value;
+    lista.appendChild(paragraph);
+    wpisz.value = "";
+    paragraph.addEventListener('click', function()
+    {
+        paragraph.style.textDecoration ="line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        lista.removeChild(paragraph);
+    })
 
 
-    var span = document.createElement("span");
-    span.id ="item_" + totalItems;
-    span.innerText = itemText; 
-
-    listItem.appendChild(checkBox);
-    listItem.appendChild(span);
-
-
-    list.appendChild(listItem);    
-    
-   
-};
-
-var totalItems = 0;
-var inItemText =document.getElementById("inItemText");
-inItemText.focus();
-inItemText.onkeyup = function(event) {
-    if(event.which == 13) {
-        var itemText = inItemText.value;
-
-        if(!itemText || itemText =="" || itemText == " ") {
-            return false;
-        }
-        addNewItem(document.getElementById("todolist"), itemText);
-        inItemText.focus();
-        inItemText.select();    
-    }
-
-
-};
+})
